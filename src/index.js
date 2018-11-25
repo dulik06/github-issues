47258e9fb9448d078a2dbae4dfebfe8a074905c1
Alicja <dul.alicja@gmail.com>
2018-11-25 12:31:24 +0100

    setting up the projects store and routs

--- src/index.js
+++ src/index.js
@@ -1,12 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { createStore, applyMiddleware, compose } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import { Provider } from 'react-redux';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import './styles/main.scss';
 import * as serviceWorker from './serviceWorker';
+import rootReducer from "./reducers";
+import { watcherSaga } from "./sagas";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Issues from './containers/Issues';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
+// create the saga middleware
+const sagaMiddleware = createSagaMiddleware();
+
+// dev tools middleware
+const reduxDevTools =
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+// create a redux store with our reducer above and middleware
+let store = createStore(
+  rootReducer,
+  compose(applyMiddleware(sagaMiddleware), reduxDevTools)
+);
+
+// run the saga
+sagaMiddleware.run(watcherSaga);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <div>
+        <Switch>
+          <Route path='/' component={Issues}/>
+        </Switch>
+      </div>
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById("root")
+);
 serviceWorker.unregister();
+
